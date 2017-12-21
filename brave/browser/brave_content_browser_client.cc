vhuarui/muon

commit 44ba11517e9967e49053174262db52a3d97ce29e

Proxy resolver Mojo files were extracted from net/ to their own service
https://crrev.com/1c14aadbf479
https://crrev.com/2df2464ba217

brave/browser/brave_content_browser_client.cc
@@ -17,6 +17,7 @@
 #include "brave/browser/notifications/platform_notification_service_impl.h"
 #include "brave/browser/password_manager/brave_password_manager_client.h"
 #include "brave/grit/brave_resources.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/cache_stats_recorder.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/plugins/plugin_info_message_filter.h"
@@ -58,6 +59,8 @@
 #include "muon/app/muon_crash_reporter_client.h"
 #include "net/base/filename_util.h"
 #include "services/data_decoder/public/interfaces/constants.mojom.h"
+#include "services/proxy_resolver/proxy_resolver_service.h"
+#include "services/proxy_resolver/public/interfaces/proxy_resolver.mojom.h"
 #include "services/service_manager/public/cpp/binder_registry.h"
 #include "third_party/WebKit/public/web/WebWindowFeatures.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -316,10 +319,22 @@ void BraveContentBrowserClient::ExposeInterfacesToRenderer(
 #endif
 }
 
+void BraveContentBrowserClient::RegisterInProcessServices(
+    StaticServiceMap* services) {
+  service_manager::EmbeddedServiceInfo info;
+  info.factory =
+      base::Bind(&proxy_resolver::ProxyResolverService::CreateService);
+  services->insert(
+      std::make_pair(proxy_resolver::mojom::kProxyResolverServiceName, info));
+}
+
 void BraveContentBrowserClient::RegisterOutOfProcessServices(
     OutOfProcessServiceMap* services) {
   (*services)[chrome::mojom::kProfileImportServiceName] =
       l10n_util::GetStringUTF16(IDS_UTILITY_PROCESS_PROFILE_IMPORTER_NAME);
+
+  (*services)[proxy_resolver::mojom::kProxyResolverServiceName] =
+      l10n_util::GetStringUTF16(IDS_UTILITY_PROCESS_PROXY_RESOLVER_NAME);
 }
 
 void BraveContentBrowserClient::BindInterfaceRequest(

BUILD.gn
@@ -257,6 +257,7 @@ source_set("utility") {
     "//chrome/common",
     "//chrome/utility",
     "//components/cookie_config",
+    "//services/proxy_resolver:lib",
     "//third_party/protobuf:protobuf_lite",
   ]
 

atom/utility/atom_content_utility_client.cc
@@ -23,9 +23,10 @@
 #include "ipc/ipc_channel.h"
 #include "ipc/ipc_message_macros.h"
 #include "mojo/public/cpp/bindings/strong_binding.h"
-#include "net/proxy/mojo_proxy_resolver_factory_impl.h"
 #include "net/proxy/proxy_resolver_v8.h"
 #include "printing/features/features.h"
+#include "services/proxy_resolver/proxy_resolver_service.h"
+#include "services/proxy_resolver/public/interfaces/proxy_resolver.mojom.h"
 #include "services/service_manager/public/cpp/binder_registry.h"
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
@@ -41,12 +42,6 @@
 namespace atom {
 
 namespace {
-
-void CreateProxyResolverFactory(
-    net::interfaces::ProxyResolverFactoryRequest request) {
-  mojo::MakeStrongBinding(base::MakeUnique<net::MojoProxyResolverFactoryImpl>(),
-                          std::move(request));
-}
 
 class ResourceUsageReporterImpl : public chrome::mojom::ResourceUsageReporter {
  public:
@@ -106,9 +101,6 @@ void AtomContentUtilityClient::UtilityThreadStarted() {
   // If our process runs with elevated privileges, only add elevated Mojo
   // interfaces to the interface registry.
   if (!utility_process_running_elevated_) {
-    registry->AddInterface<net::interfaces::ProxyResolverFactory>(
-        base::Bind(CreateProxyResolverFactory),
-        base::ThreadTaskRunnerHandle::Get());
     registry->AddInterface(base::Bind(CreateResourceUsageReporter),
                            base::ThreadTaskRunnerHandle::Get());
   }
@@ -132,6 +124,14 @@ bool AtomContentUtilityClient::OnMessageReceived(
 
 void AtomContentUtilityClient::RegisterServices(
     AtomContentUtilityClient::StaticServiceMap* services) {
+  service_manager::EmbeddedServiceInfo proxy_resolver_info;
+  proxy_resolver_info.task_runner =
+      content::ChildThread::Get()->GetIOTaskRunner();
+  proxy_resolver_info.factory =
+      base::Bind(&proxy_resolver::ProxyResolverService::CreateService);
+  services->emplace(proxy_resolver::mojom::kProxyResolverServiceName,
+                    proxy_resolver_info);
+
   service_manager::EmbeddedServiceInfo profile_import_info;
   profile_import_info.factory =
     base::Bind(&BraveProfileImportService::CreateService);

brave/browser/brave_content_browser_client.h
@@ -52,6 +52,7 @@ class BraveContentBrowserClient : public atom::AtomBrowserClient {
       service_manager::BinderRegistry* registry,
       content::AssociatedInterfaceRegistry* associated_registry,
       content::RenderProcessHost* render_process_host) override;
+  void RegisterInProcessServices(StaticServiceMap* services) override;
   void RegisterOutOfProcessServices(OutOfProcessServiceMap* services) override;
   void BindInterfaceRequestFromFrame(
       content::RenderFrameHost* render_frame_host,

vendor/brightray/browser/url_request_context_getter.cc
@@ -17,6 +17,7 @@
 #include "chrome/common/chrome_switches.h"
 #include "common/switches.h"
 #include "components/cookie_config/cookie_store_util.h"
+#include "content/network/proxy_service_mojo.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/cookie_store_factory.h"
 #include "content/public/browser/devtools_network_transaction_factory.h"
@@ -39,7 +40,6 @@
 #include "net/proxy/proxy_config_service.h"
 #include "net/proxy/proxy_script_fetcher_impl.h"
 #include "net/proxy/proxy_service.h"
-#include "net/proxy/proxy_service_mojo.h"
 #include "net/ssl/channel_id_service.h"
 #include "net/ssl/default_channel_id_store.h"
 #include "net/ssl/ssl_config_service_defaults.h"
@@ -252,13 +252,12 @@ net::URLRequestContext* URLRequestContextGetter::GetURLRequestContext() {
         net::DhcpProxyScriptFetcherFactory dhcp_factory;
         dhcp_proxy_script_fetcher = dhcp_factory.Create(url_request_context_.get());
 
-        proxy_service = net::CreateProxyServiceUsingMojoFactory(
-              ChromeMojoProxyResolverFactory::GetInstance(),
-              std::move(proxy_config_service_),
-              new net::ProxyScriptFetcherImpl(url_request_context_.get()),
-              std::move(dhcp_proxy_script_fetcher), host_resolver.get(),
-              net_log_,
-              url_request_context_->network_delegate());
+        proxy_service = content::CreateProxyServiceUsingMojoFactory(
+            ChromeMojoProxyResolverFactory::GetInstance(),
+            std::move(proxy_config_service_),
+            new net::ProxyScriptFetcherImpl(url_request_context_.get()),
+            std::move(dhcp_proxy_script_fetcher), host_resolver.get(), net_log_,
+            url_request_context_->network_delegate());
       } else {
         proxy_service = net::ProxyService::CreateUsingSystemProxyResolver(
             std::move(proxy_config_service_),

commit 22918ae0c2b5b55d82ca8b10be9884e0a13efa30

Remove wrong proxy resolver service code
This was wrongly introduced in 44ba11517e99. This code is duplicated
from //electron/atom/utility/atom_content_utility_client.cc.

brave/browser/brave_content_browser_client.cc
@@ -62,7 +62,6 @@
 #include "muon/app/muon_crash_reporter_client.h"
 #include "net/base/filename_util.h"
 #include "services/data_decoder/public/interfaces/constants.mojom.h"
-#include "services/proxy_resolver/proxy_resolver_service.h"
 #include "services/proxy_resolver/public/interfaces/proxy_resolver.mojom.h"
 #include "services/service_manager/public/cpp/binder_registry.h"
 #include "third_party/WebKit/public/web/WebWindowFeatures.h"
@@ -320,11 +319,6 @@ void BraveContentBrowserClient::RegisterInProcessServices(
     info.factory = base::Bind(&ChromeService::Create);
     services->insert(std::make_pair(chrome::mojom::kServiceName, info));
   }
-  service_manager::EmbeddedServiceInfo info;
-  info.factory =
-      base::Bind(&proxy_resolver::ProxyResolverService::CreateService);
-  services->insert(
-      std::make_pair(proxy_resolver::mojom::kProxyResolverServiceName, info));
 }
 
 void BraveContentBrowserClient::RegisterOutOfProcessServices(